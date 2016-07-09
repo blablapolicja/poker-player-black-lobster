19536d9a98a06b5425c9ef14c8d13020972a925c
blablapolicja <blablapolicja@gmail.com>
2016-07-09 11:27:27 +0300

    always call

--- player.js
+++ player.js
@@ -4,7 +4,12 @@ module.exports = {
   VERSION: "0.0.1",
 
   bet_request: function(game_state, bet) {
-    bet(0);
+    var myBet = 0,
+        myIndex = game_state.in_action;
+    
+    myBet = game_state.current_buy_in - game_state.players[myIndex].bet;
+    
+    bet(myBet);
   },
 
   showdown: function(game_state) {
